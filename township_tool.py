4f4211a68e45d74b2016147970c0bfe29d89470f
TappandoPlay <sfruttailmomento94@tutamail.com>
2025-06-15 02:17:11 +0200

    Create township_tool.py

---
+++ township_tool.py
@@ -0,0 +1,6 @@
+# Simulazione tool Township
+def simulate_money_generation():
+    print("Township Helper Avviato - Banconote in arrivo!")
+
+if __name__ == "__main__":
+    simulate_money_generation()
